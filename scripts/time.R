389aa228843b606949068b926acd8ee0b72ed829
Nancy Liu <nsliu@D-10-19-199-141.dhcp4.washington.edu>
2019-03-13 00:11:21 -0700

    formatting

--- scripts/time.R
+++ scripts/time.R
@@ -4,30 +4,29 @@ library(lubridate)
 library(stringr)
 
 time <- function(period){
-  # output$time <- renderTable({
+  crime_another <- read.csv("data/crime_modified.csv", stringsAsFactors = FALSE)
+  crime_clean <- crime_another %>%
+    select(Occurred.Time, Crime.Subcategory, Neighborhood)
     
-    crime_clean <- crime_modified %>%
-      select(Occurred.Time, Crime.Subcategory, Neighborhood)
+  crime_clean$Time.Of.Day <- ifelse(crime_clean$Occurred.Time >= 0 & crime_clean$Occurred.Time <= 559, 'Early Morning',
+                                    ifelse(crime_clean$Occurred.Time >= 600 & crime_clean$Occurred.Time <= 1059, 'Morning',
+                                           ifelse(crime_clean$Occurred.Time >= 1100 & crime_clean$Occurred.Time <= 1359, 'Noon',
+                                                  ifelse(crime_clean$Occurred.Time >= 1400 & crime_clean$Occurred.Time <= 1759, 'Afternoon',
+                                                         ifelse(crime_clean$Occurred.Time >= 1800 & crime_clean$Occurred.Time <= 2359, 'Night', 'No Time Specified')))))
+  crime_time <- crime_clean %>%
+    filter(Time.Of.Day == period)
     
-    crime_clean$Time.Of.Day <- ifelse(crime_clean$Occurred.Time >= 0 & crime_clean$Occurred.Time <= 559, 'Early Morning',
-                                      ifelse(crime_clean$Occurred.Time >= 600 & crime_clean$Occurred.Time <= 1059, 'Morning',
-                                             ifelse(crime_clean$Occurred.Time >= 1100 & crime_clean$Occurred.Time <= 1359, 'Noon',
-                                                    ifelse(crime_clean$Occurred.Time >= 1400 & crime_clean$Occurred.Time <= 1759, 'Afternoon',
-                                                           ifelse(crime_clean$Occurred.Time >= 1800 & crime_clean$Occurred.Time <= 2359, 'Night', 'No Time Specified')))))
-    crime_time <- crime_clean %>%
-      filter(Time.Of.Day == period)
-    
-   Crime.Number <- summary(as.factor(crime_time$Crime.Subcategory))
-   Most.Crime <- sort(as.factor(Crime.Number), decreasing = TRUE)
-   Most.Crime <- data.frame(Most.Crime)
-   Most.Crime <- row.names(Most.Crime)
-   Most.Crime <- data.frame(Most.Crime)
+  Crime.Number <- summary(as.factor(crime_time$Crime.Subcategory))
+  Most.Crime <- sort(as.factor(Crime.Number), decreasing = TRUE)
+  Most.Crime <- data.frame(Most.Crime)
+  Most.Crime <- row.names(Most.Crime)
+  Most.Crime <- data.frame(Most.Crime)
    
-   Neighborhood.Number <- summary(as.factor(crime_time$Neighborhood))
-   Most.Neighborhood <- sort(as.factor(Neighborhood.Number), decreasing = TRUE)
-   Most.Neighborhood <- data.frame(Most.Neighborhood)
-   Most.Neighborhood <- row.names(Most.Neighborhood)
-   Most.Neighborhood <- data.frame(Most.Neighborhood)
+  Neighborhood.Number <- summary(as.factor(crime_time$Neighborhood))
+  Most.Neighborhood <- sort(as.factor(Neighborhood.Number), decreasing = TRUE)
+  Most.Neighborhood <- data.frame(Most.Neighborhood)
+  Most.Neighborhood <- row.names(Most.Neighborhood)
+  Most.Neighborhood <- data.frame(Most.Neighborhood)
    
   most_neighborhood <- head(Most.Neighborhood, 5)
   most_crime <- head(Most.Crime, 5)
@@ -39,6 +38,6 @@ time <- function(period){
   colnames(summary) <- c("Neighborhood w/ Most Crime", "Most Common Crime", "Neighborhood w/ Least Crime", "Least Common Crime")
   
   summary
-  
-  # })
+
 }
+
